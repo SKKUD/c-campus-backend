1378b5b88fc82109a2cf885bd857d8c8243d8b4a
whyjyj <yoon_jae___z@naver.com>
2023-07-16 00:59:33 +0900

    [Feat] Entity

---
+++ src/main/java/edu/skku/cc/domain/Quiz.java
@@ -0,0 +1,19 @@
+package edu.skku.cc.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Quiz {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "quiz")
+    private Message message;
+
+    @Column(length = 20)
+    private String content;
+
+    private String answer;
+
+    private Boolean isSolved;
+}
---
+++ src/main/java/edu/skku/cc/domain/User.java
@@ -0,0 +1,35 @@
+package edu.skku.cc.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Message> messages;
+
+    public Integer getMessageCount() {
+        return messages.size();
+    }
+}
+
+
