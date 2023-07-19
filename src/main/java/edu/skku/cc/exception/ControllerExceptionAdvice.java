85a601a74b28a08113a5244174eb5ad7bf33fa5d
whyjyj <yoon_jae___z@naver.com>
2023-07-19 17:46:41 +0900

    [Feat] Error Handling

---
+++ src/main/java/edu/skku/cc/exception/ControllerExceptionAdvice.java
@@ -0,0 +1,24 @@
+package edu.skku.cc.exception;
+
+import edu.skku.cc.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Component
+@RequiredArgsConstructor
+public class ControllerExceptionAdvice
+{
+    /**
+     * CUSTOM_ERROR
+     */
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ApiResponse> handleCustomException(CustomException e) {
+        return ResponseEntity.status(e.getHttpStatus())
+                .body(ApiResponse.error(e.getErrorType(), e.getMessage()));
+    }
+}
