df3bb0f22687b2091d6ae90b88aaed6987f86141
wndyd <wndyd0131@naver.com>
2023-07-25 18:19:21 +0900

    Jwt authentication ready

--- src/main/java/edu/skku/cc/jwt/JwtTokenUtil.java
+++ src/main/java/edu/skku/cc/jwt/JwtTokenUtil.java
@@ -1,22 +1,33 @@
 package edu.skku.cc.jwt;
 
 import edu.skku.cc.enums.JwtExpirationTime;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class JwtTokenUtil {
     @Value("${jwt.secret}")
     private String SECRET_KEY;
+    protected static final String AUTHORITIES_KEY = "auth";
     private long ACCESS_TOKEN_EXPIRATION_TIME = JwtExpirationTime.ACCESS_TOKEN_EXPIRATION_TIME.getExpirationTime();
     private long REFRESH_TOKEN_EXPIRE_TIME = JwtExpirationTime.REFRESH_TOKEN_EXPIRATION_TIME.getExpirationTime();
 
@@ -38,6 +49,42 @@ public class JwtTokenUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + expireTime))    // JWT 토큰 만료 시간
                 .signWith(new SecretKeySpec(SECRET_KEY.getBytes(), SignatureAlgorithm.HS512.getJcaName()))   // HS512 알고리즘을 사용하여 secretKey를 이용해 서명
                 .compact(); // JWT 토큰 생성
+    }
+
+    public Authentication getAuthenticationFromToken(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        String email = String.valueOf(claims.get("email"));
+        log.info("email: {}", email);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, token);
+        log.info("authentication: {}", authentication.getPrincipal());
+        return new UsernamePasswordAuthenticationToken(email, token);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts
+                    .parserBuilder()
+                    .setSigningKey(SECRET_KEY.getBytes())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
     }
 
     public long getRefreshTokenExpireTime() {
