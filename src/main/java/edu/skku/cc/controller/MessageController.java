4a7e40f44ed4449a9aa984b8755aa21b6a09cf6e
Seungil Kim <ksi990302@gmail.com>
2023-07-28 13:11:52 +0900

    [Update] MessageResponseDto로 변경 및 createMessage 메서드 추가

--- src/main/java/edu/skku/cc/controller/MessageController.java
+++ src/main/java/edu/skku/cc/controller/MessageController.java
@@ -1,16 +1,18 @@
 package edu.skku.cc.controller;
 
 import edu.skku.cc.dto.ApiResponse;
-import edu.skku.cc.dto.Message.MessageListResponseDto;
+import edu.skku.cc.dto.Message.CreateMessageRequestDto;
 import edu.skku.cc.dto.Message.MessagePublicUpdateResponseDto;
+import edu.skku.cc.dto.Message.MessageResponseDto;
 import edu.skku.cc.dto.Message.MessageSolveQuizRequestDto;
-import edu.skku.cc.dto.Message.SingleMessageResponseDto;
 import edu.skku.cc.exception.SuccessType;
 import edu.skku.cc.service.MessageService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -21,13 +23,13 @@ public class MessageController {
     private final MessageService messageService;
 
     @GetMapping("/users/{userId}/messages/pulled")
-    public ApiResponse<List<MessageListResponseDto>> getUserPulledMessageList(@PathVariable Long userId) {
+    public ApiResponse<List<MessageResponseDto>> getUserPulledMessageList(@PathVariable Long userId) {
         return ApiResponse.success(SuccessType.GET_USER_MESSAGE_ALL_SUCCESS, messageService.getUserPulledMessageList(userId));
     }
 
     @PreAuthorize("@webSecurity.checkAuthority(authentication, #userId)")
     @GetMapping("/users/{userId}/messages/{messageId}")
-    public ApiResponse<SingleMessageResponseDto> getSingleUserMessage(@PathVariable Long userId, @PathVariable Long messageId, Authentication authentication) {
+    public ApiResponse<MessageResponseDto> getSingleUserMessage(@PathVariable Long userId, @PathVariable Long messageId, Authentication authentication) {
         return ApiResponse.success(SuccessType.GET_USER_MESSAGE_ONE_SUCCESS, messageService.getSingleUserMessage(userId, messageId));
     }
 
@@ -49,8 +51,13 @@ public class MessageController {
         return ApiResponse.success(SuccessType.PULL_USER_MESSAGE_SUCCESS, messageService.pullMessage(userId));
     }
 
+    @PostMapping("/users/{userId}/messages")
+    public ApiResponse<Long> createMessage(@PathVariable Long userId, @RequestPart @Valid CreateMessageRequestDto request, @RequestPart(value = "file", required = false) MultipartFile file) {
+        return ApiResponse.success(SuccessType.CREATE_MESSAGE_SUCCESS, messageService.createMessage(userId, request, file));
+    }
+
     @PostMapping("/users/{userId}/messages/{messageId}/quiz")
-    public ApiResponse solveQuiz(@PathVariable Long userId, @PathVariable Long messageId, @RequestBody MessageSolveQuizRequestDto request) {
+    public ApiResponse solveQuiz(@PathVariable Long userId, @PathVariable Long messageId, @RequestBody @Valid MessageSolveQuizRequestDto request) {
         messageService.solveMessageQuiz(userId, messageId, request.getAnswer());
         return ApiResponse.success(SuccessType.SOLVE_MESSAGE_QUIZ_SUCCESS);
     }
