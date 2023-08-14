615bcf20ea586809fdd48172a4bcfa0a8c6880e9
wndyd <wndyd0131@naver.com>
2023-08-14 17:53:09 +0900

    AuthController response null

--- src/main/java/edu/skku/cc/controller/AuthController.java
+++ src/main/java/edu/skku/cc/controller/AuthController.java
@@ -3,6 +3,7 @@ package edu.skku.cc.controller;
 import edu.skku.cc.dto.auth.AuthenticationResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -10,15 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import software.amazon.awssdk.http.HttpStatusCode;
 
 @Controller
+@Slf4j
 public class AuthController {
     @GetMapping("/auth/authentication")
     public ResponseEntity<AuthenticationResponseDto> checkAuthentication(HttpServletRequest request, Authentication authentication) {
         HttpSession session = request.getSession(false);
+        log.info("session {}", session.getAttribute("userId"));
         if (session != null && authentication != null) {
             String userId = String.valueOf(authentication.getPrincipal());
             return ResponseEntity.status(HttpStatusCode.OK).body(new AuthenticationResponseDto(Long.parseLong(userId)));
         } else {
-            return ResponseEntity.status(HttpStatusCode.UNAUTHORIZED).body(new AuthenticationResponseDto(null));
+            return ResponseEntity.status(HttpStatusCode.OK).body(new AuthenticationResponseDto(null));
         }
     }
 }
